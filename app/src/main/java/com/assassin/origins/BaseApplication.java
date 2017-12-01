ad4dfb380c858df651f4a9c26aeb4e581dd3eeaf
杨超 <yangchao@babyfs.cn>
2017-12-01 16:03:25 +0800

    re-init

---
+++ app/src/main/java/com/assassin/origins/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.assassin.origins;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by yc-work on 2017/12/1 下午3:35.
+ */
+public class BaseApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
