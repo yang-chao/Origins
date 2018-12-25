96fdbf2cb0c64a82a14f3ba2f6ed39cdf75f88f6
YC-IMAC <yangchao@babyfs.cn>
2018-12-25 10:30:31 +0800

    init splash project

---
+++ splash/src/main/java/com/assassin/origins/splash/AcMainActivity.kt
@@ -0,0 +1,12 @@
+package com.assassin.origins.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ac_main)
+    }
+}
