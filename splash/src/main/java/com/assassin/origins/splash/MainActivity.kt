730bcdbb63a259ebb1bb2f125764fe669c398499
YC-IMAC <yangchao@babyfs.cn>
2018-12-25 10:43:24 +0800

    init splash project

--- splash/src/main/java/com/assassin/origins/splash/MainActivity.kt
+++ splash/src/main/java/com/assassin/origins/splash/MainActivity.kt
@@ -3,7 +3,7 @@ package com.assassin.origins.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AcMainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
